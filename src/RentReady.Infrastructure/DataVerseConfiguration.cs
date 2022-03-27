e34038c51991dc28db3f2e2a0677cd7484942cb9
Abel Beneberu <abel.beneberu@betssongroup.com>
2022-03-27 23:36:29 +0300

    update DataVerseConfiguration to simplified version

--- src/RentReady.Infrastructure/DataVerseConfiguration.cs
+++ src/RentReady.Infrastructure/DataVerseConfiguration.cs
@@ -2,12 +2,6 @@
 {
 	public class DataVerseConfiguration
 	{
-		public string ClientId { get; set; } = string.Empty; 
-		public string UserName { get; set; } = string.Empty;
-		public string Password { get; set; } = string.Empty;
-		public string ApplicationUrl { get; set; } = string.Empty;
-		public string RedirectUrl { get; set; } = string.Empty;
-		public string ApiVersion { get; set; } = string.Empty;
 		public string ConnectionString { get; set; } = string.Empty;
 	}
 }
